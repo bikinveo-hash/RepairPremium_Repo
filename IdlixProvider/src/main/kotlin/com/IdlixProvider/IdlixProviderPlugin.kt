746cac0af365cc857c7eb9dd9120fdcd7d73c505
bikinveo-hash <bikinveo@gmail.com>
2026-04-03 08:00:29 +0900

    Update IdlixProviderPlugin.kt

--- IdlixProvider/src/main/kotlin/com/IdlixProvider/IdlixProviderPlugin.kt
+++ IdlixProvider/src/main/kotlin/com/IdlixProvider/IdlixProviderPlugin.kt
@@ -1,11 +1,10 @@
-package com.lagradost.cloudstream3.plugins // Sesuaikan nama package dengan struktur folder proyekmu!
+package com.lagradost.cloudstream3.plugins
 
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
 
 @CloudstreamPlugin
 class IdlixProviderPlugin : Plugin() {
-    // Tuliskan android.content.Context secara langsung di sini tanpa melakukan import di atas
     override fun load(context: android.content.Context) {
         // Mendaftarkan API Website IDLIX
         registerMainAPI(IdlixProvider())
