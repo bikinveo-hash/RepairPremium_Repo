7d8ccb9b658eb500527065b7e95840656473a53d
bikinveo-hash <bikinveo@gmail.com>
2026-04-03 16:37:50 +0900

    Update KisskhProviderPlugin.kt

--- KisskhProvider/src/main/kotlin/com/michat88/KisskhProviderPlugin.kt
+++ KisskhProvider/src/main/kotlin/com/michat88/KisskhProviderPlugin.kt
@@ -6,7 +6,7 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 @CloudstreamPlugin
 class KisskhProviderPlugin: BasePlugin() {
     override fun load() {
-        // Mendaftarkan provider Kisskh ke dalam sistem
+        // Mendaftarkan API Provider
         registerMainAPI(KisskhProvider())
     }
 }
