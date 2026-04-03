0a2e3f7828002259bafeaf29dd57a1a02114eadc
bikinveo-hash <bikinveo@gmail.com>
2026-04-03 17:09:09 +0900

    Update build.gradle.kts

--- KisskhProvider/build.gradle.kts
+++ KisskhProvider/build.gradle.kts
@@ -1,19 +1,5 @@
-
-import org.jetbrains.kotlin.konan.properties.Properties
 // use an integer for version numbers
 version = 17
-
-android {
-    defaultConfig {
-        val properties = Properties()
-        properties.load(project.rootProject.file("local.properties").inputStream())
-        android.buildFeatures.buildConfig=true
-        buildConfigField("String", "KissKh", "\"${properties.getProperty("KissKh")}\"")
-        buildConfigField("String", "KisskhSub", "\"${properties.getProperty("KisskhSub")}\"")
-
-    }
-}
-
 
 cloudstream {
     language = "en"
