3b596e6997bf4439190807623ab3929ffdb19f43
bikinveo-hash <bikinveo@gmail.com>
2026-04-17 23:08:26 +0900

    Create SflixPlugin.kt

---
+++ Sflix/src/main/kotlin/com/sflix/SflixPlugin.kt
@@ -0,0 +1,13 @@
+package com.sflix
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class SflixPlugin : Plugin() {
+    override fun load(context: Context) {
+        // Mendaftarkan class Sflix sebagai MainAPI ke dalam CloudStream
+        registerMainAPI(Sflix())
+    }
+}
