c647c24917aa0ea177f83bb2ba88d16319454f7a
bikinveo-hash <bikinveo@gmail.com>
2026-04-22 16:19:24 +0900

    Update IdlixProviderPlugin.kt

--- IdlixProvider/src/main/kotlin/com/IdlixProvider/IdlixProviderPlugin.kt
+++ IdlixProvider/src/main/kotlin/com/IdlixProvider/IdlixProviderPlugin.kt
@@ -7,7 +7,6 @@ import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 class IdlixProviderPlugin: BasePlugin() {
     override fun load() {
         registerMainAPI(IdlixProvider())
-        [span_7](start_span)registerExtractorAPI(Jeniusplay()) // Tetap ada untuk film lama[span_7](end_span)
-        registerExtractorAPI(Majorplay())  // Ekstraktor baru
+        registerExtractorAPI(Majorplay())
     }
 }
