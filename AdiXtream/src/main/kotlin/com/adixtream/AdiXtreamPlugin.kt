29f9453fa10030d65352bb26ae443a82a6ed828b
bikinveo-hash <bikinveo@gmail.com>
2026-04-24 06:03:17 +0900

    Update AdiXtreamPlugin.kt

--- AdiXtream/src/main/kotlin/com/adixtream/AdiXtreamPlugin.kt
+++ AdiXtream/src/main/kotlin/com/adixtream/AdiXtreamPlugin.kt
@@ -6,17 +6,8 @@ import android.content.Context
 
 @CloudstreamPlugin
 class AdiXtreamPlugin : Plugin() {
-    
-    /**
-     * Fungsi 'load' ini adalah hal pertama yang dijalankan oleh Cloudstream 
-     * saat ekstensi/plugin-mu berhasil dipasang.
-     */
     override fun load(context: Context) {
-        // Mendaftarkan provider VidSrcProvider yang sudah kita buat sebelumnya
-        registerMainAPI(AdiXtreamProvider())
-        
-        // Catatan: Jika suatu saat nanti kamu membuat file provider lain 
-        // (misalnya NontonGratisProvider.kt), kamu tinggal menambahkannya di bawah ini:
-        // registerMainAPI(NontonGratisProvider())
+        // Mendaftarkan provider VidSrcProvider agar dikenali oleh Cloudstream
+        registerMainAPI(VidSrcProvider())
     }
 }
