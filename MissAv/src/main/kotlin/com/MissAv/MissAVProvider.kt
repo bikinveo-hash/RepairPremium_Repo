f82585eeeb44ee3338093cf7cabb21557fff84ba
bikinveo-hash <bikinveo@gmail.com>
2026-05-03 19:22:49 +0900

    Update MissAVProvider.kt

--- MissAv/src/main/kotlin/com/MissAv/MissAVProvider.kt
+++ MissAv/src/main/kotlin/com/MissAv/MissAVProvider.kt
@@ -12,8 +12,11 @@ class MissAvProvider : MainAPI() {
     override val supportedTypes = setOf(TvType.NSFW)
     override val hasDownloadSupport = true
     override var sequentialMainPage = true
+    
+    // KUNCI SAKTI: Memaksa Cloudstream menggunakan Browser Asli (WebView)
+    // untuk melewati blokir Cloudflare / Anti-Bot
+    override val usesWebView = true
 
-    // Headers penyamaran agar tidak dicegat oleh sistem anti-bot (Cloudflare)
     private val headers = mapOf(
         "User-Agent" to "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
         "Accept" to "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
@@ -35,7 +38,6 @@ class MissAvProvider : MainAPI() {
             "$mainUrl/id/${request.data}?page=$page"
         }
         
-        // Memasukkan headers ke dalam request app.get
         val document = app.get(url, headers = headers).document
         
         val home = document.select("div.thumbnail").mapNotNull { 
