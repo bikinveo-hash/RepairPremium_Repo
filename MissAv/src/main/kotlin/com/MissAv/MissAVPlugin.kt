0dc60a4df98d08629a4331b7ec3e90bfc67ed1a0
bikinveo-hash <bikinveo@gmail.com>
2026-05-03 18:58:44 +0900

    Update MissAVPlugin.kt

--- MissAv/src/main/kotlin/com/MissAv/MissAVPlugin.kt
+++ MissAv/src/main/kotlin/com/MissAv/MissAVPlugin.kt
@@ -5,9 +5,9 @@ import com.lagradost.cloudstream3.plugins.Plugin
 import android.content.Context
 
 @CloudstreamPlugin
-class MissAVPlugin: Plugin() {
+class MissAvPlugin : Plugin() {
     override fun load(context: Context) {
-        // Pastikan nama class di sini sama persis dengan di file Provider (MissAVProvider)
-        registerMainAPI(MissAVProvider())
+        // Daftarkan Provider kita di sini
+        registerMainAPI(MissAvProvider())
     }
 }
