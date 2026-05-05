50c9cca6c94d83d031c26feb036d8255c7c30c23
bikinveo-hash <bikinveo@gmail.com>
2026-05-05 19:52:37 +0900

    Update FreeReelsPlugin.kt

--- FreeReels/src/main/kotlin/com/FreeReels/FreeReelsPlugin.kt
+++ FreeReels/src/main/kotlin/com/FreeReels/FreeReelsPlugin.kt
@@ -1,13 +1,13 @@
-package com.lagradost.cloudstream3.plugins
+package com.FreeReels
 
+import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
-import android.content.Context
 
 @CloudstreamPlugin
 class FreeReelsPlugin : Plugin() {
     override fun load(context: Context) {
-        // Mendaftarkan provider FreeReels
+        // Mendaftarkan provider FreeReels ke dalam aplikasi
         registerMainAPI(FreeReels())
     }
 }
