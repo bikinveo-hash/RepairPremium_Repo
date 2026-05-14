dd58753ec34649acf1a35ac9b4f4a2bee667b477
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-05-14 20:13:41 +0900

    Update LayarKacaProviderPlugin.kt

--- LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
+++ LayarKacaProvider/src/main/kotlin/com/LayarKacaProvider/LayarKacaProviderPlugin.kt
@@ -7,8 +7,10 @@ import android.content.Context
 @CloudstreamPlugin
 class LayarKacaPlugin : Plugin() {
     override fun load(context: Context) {
+        // Mendaftarkan Provider Utama
         registerMainAPI(LayarKacaProvider())
         
+        // Mendaftarkan 3 Extractor Utama (Tanpa Hydrax)
         registerExtractorAPI(P2PExtractor())
         registerExtractorAPI(EmturbovidExtractor())
         registerExtractorAPI(F16Extractor())
