545ce16588e3e657faeea41d9bf7b872d4716c27
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-05-23 02:32:37 +0900

    Update Majorplay.kt

--- IdlixProvider/src/main/kotlin/com/IdlixProvider/Majorplay.kt
+++ IdlixProvider/src/main/kotlin/com/IdlixProvider/Majorplay.kt
@@ -2,9 +2,8 @@ package com.IdlixProvider
 
 import com.fasterxml.jackson.annotation.JsonProperty
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties
-import com.lagradost.cloudstream3.*
-import com.lagradost.cloudstream3.utils.*
-import com.lagradost.cloudstream3.utils.AppUtils.toJson
+import com.lagradost.cloudstream3.* import com.lagradost.cloudstream3.utils.*
+import com.lagradost.cloudstream3.utils.AppUtils.toJson 
 import okhttp3.MediaType.Companion.toMediaTypeOrNull
 import okhttp3.RequestBody.Companion.toRequestBody
 
@@ -22,7 +21,6 @@ class Majorplay : ExtractorApi() {
         val claimToken = url.substringAfter("claim=").substringBefore("&")
         if (claimToken.isEmpty()) return
 
-        // PENYELARASAN AGAR MENGGUNAKAN USER-AGENT YANG PERSIS SAMA DENGAN IDLIXPROVIDER
         val safeHeaders = mapOf(
             "Origin" to "https://z1.idlixku.com",
             "Referer" to "https://z1.idlixku.com/",
@@ -32,7 +30,6 @@ class Majorplay : ExtractorApi() {
         val textMediaType = "text/plain".toMediaTypeOrNull()
         val requestBodyData = mapOf("claim" to claimToken).toJson().toRequestBody(textMediaType)
 
-        // Penambahan Content-Type text/plain secara eksplisit ke dalam header map
         val response = app.post(
             url = "$mainUrl/api/play",
             headers = safeHeaders.plus("Content-Type" to "text/plain"),
@@ -41,7 +38,7 @@ class Majorplay : ExtractorApi() {
 
         val masterConfigUrl = response.url ?: return
         
-        // RESTRUKTURISASI KE PERULANGAN TRADISIONAL 'FOR' UNTUK MENGEKSEKUSI SUSPEND METHOD SECARA SAH
+        // Pemuatan file subtitle resmi bawaan web secara tradisional
         val subtitles = response.subtitles
         if (subtitles != null) {
             for (sub in subtitles) {
@@ -53,12 +50,14 @@ class Majorplay : ExtractorApi() {
             }
         }
         
-        // Pengiriman link manifest m3u8 terselubung langsung ke inti ExoPlayer
+        // TRIK FINISH: Menambahkan &.m3u8 di akhir agar lolos dari penyaringan Cloudstream
+        val finalPlayableUrl = "$masterConfigUrl&.m3u8"
+        
         callback.invoke(
             newExtractorLink(
                 source = name,
                 name = "Majorplay Auto Quality",
-                url = masterConfigUrl,
+                url = finalPlayableUrl,
                 type = ExtractorLinkType.M3U8
             ) {
                 this.headers = safeHeaders
