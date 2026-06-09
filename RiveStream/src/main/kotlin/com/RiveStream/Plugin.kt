9a0a4f7bafcd1c110ecd5b7838c15c654cc4428c
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-06-09 22:17:36 +0900

    Create Plugin.kt

---
+++ RiveStream/src/main/kotlin/com/RiveStream/Plugin.kt
@@ -0,0 +1,13 @@
+package com.RiveStream
+
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
+
+@CloudstreamPlugin
+class RiveStreamPlugin: Plugin() {
+    override fun load(context: Context) {
+        // Mendaftarkan provider utama RiveStream ke sistem core
+        registerMainAPI(RiveStreamProvider())
+    }
+}
