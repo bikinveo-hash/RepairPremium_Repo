9a6f7fef3ca446b8304f0686d7b88eb50cc57273
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-06-11 05:21:46 +0900

    Update PrimeSrcHelper.kt

--- RiveStream/src/main/kotlin/com/RiveStream/PrimeSrcHelper.kt
+++ RiveStream/src/main/kotlin/com/RiveStream/PrimeSrcHelper.kt
@@ -2,68 +2,58 @@ package com.RiveStream
 
 import com.lagradost.cloudstream3.*
 import com.lagradost.cloudstream3.utils.*
-import kotlinx.coroutines.delay
 
 class PrimeSrcHelper {
 
     suspend fun invokePrimeSrc(
-        data: String,
+        data: String, // Berisi URL tujuan asli (contoh: https://www.rivestream.app/movie/XXXXXX)
         mainUrl: String,
         providerName: String,
         subtitleCallback: (SubtitleFile) -> Unit,
         callback: (ExtractorLink) -> Unit
     ): Boolean {
         
-        val cleanData = data.replace("$mainUrl/", "")
-        val isMovie = !cleanData.contains("?s=")
-        val cleanId = cleanData.substringAfter("/").substringBefore("?")
+        // REGEX RADAR: Menangkap momen krusial saat cdn hulu atau host video pihak ketiga mulai dimuat
+        val playerRegex = Regex(".*(1shows|cdn\\.1shows\\.app|streamtape|voe|streamwish|filemoon|dood|mixdrop|streamplay|vinovo|vidmoly).*")
 
-        val embedUrl = if (isMovie) {
-            "https://primesrc.me/embed/movie?tmdb=$cleanId"
-        } else {
-            val season = cleanData.substringAfter("?s=").substringBefore("&")
-            val episode = cleanData.substringAfter("&e=")
-            "https://primesrc.me/embed/tv?tmdb=$cleanId&season=$season&episode=$episode"
-        }
-
-        val serversUrl = if (isMovie) {
-            "https://primesrc.me/api/v1/s?tmdb=$cleanId&type=movie"
-        } else {
-            val season = cleanData.substringAfter("?s=").substringBefore("&")
-            val episode = cleanData.substringAfter("&e=")
-            "https://primesrc.me/api/v1/s?tmdb=$cleanId&type=tv&season=$season&episode=$episode"
-        }
-
-        // REGEX SAKTI: Ditambahkan "1shows" ke dalam daftar intersepsi radar WebView
-        val playerRegex = Regex(".*(1shows|streamtape|voe|streamwish|filemoon|dood|mixdrop|streamplay|vinovo|vidmoly|vidara|savefiles|filelions|luluvdoo|streamruby|vidsst|mshare|uqload|fembed).*")
+        // SKRIP JINAK: Disuntikkan ke dalam WebView untuk menetralisir 'disable-devtools' agar tidak membekukan mesin JS
+        val bypassAntiDebugScript = """
+            Object.defineProperty(window, 'devtools', { get: function() { return { isOpen: false }; } });
+            Function.prototype.constructor = function(str) { 
+                if (str === 'debugger') return function(){}; 
+                return Function(str); 
+            };
+        """.trimIndent()
 
         var linksFound = 0
 
         try {
+            // Jalankan WebView langsung mengarah ke halaman frontend utama seperti isi DevTools kamu
             val webViewResolver = com.lagradost.cloudstream3.network.WebViewResolver(
                 interceptUrl = playerRegex,
-                userAgent = null, 
-                useOkhttp = false 
+                userAgent = null, // Menggunakan User-Agent bawaan mesin Chromium HP agar Turnstile lolos
+                useOkhttp = false,
+                script = bypassAntiDebugScript // Menyuntikkan penjinak anti-debug
             )
 
+            // WebView memproses halaman secara natural di latar belakang
             val (interceptedRequest, _) = webViewResolver.resolveUsingWebView(
-                url = embedUrl,
+                url = data, // KUNCI: Muat langsung rivestream.app/movie/... bukan embed primesrc
                 referer = "$mainUrl/"
             )
 
             val realEmbedUrl = interceptedRequest?.url?.toString()
 
             if (!realEmbedUrl.isNullOrBlank()) {
-                
-                // KUNCI UTAMA: Jika yang tertangkap adalah server 1shows, bypass direct ke ExoPlayer dengan headers
+                // KUNCI UTAMA: Jika cdn utama '1shows' yang berhasil kita tangkap lewat radar
                 if (realEmbedUrl.contains("1shows")) {
                     callback(newExtractorLink(
                         source = providerName,
-                        name = "RiveStream CDN (Multi-Quality)",
+                        name = "RiveStream CDN",
                         url = realEmbedUrl,
-                        type = ExtractorLinkType.M3U8 // Deklarasikan sebagai tipe M3U8 resmi
+                        type = ExtractorLinkType.M3U8
                     ) {
-                        // Suntikkan headers persis dengan parameter curl yang kamu temukan
+                        // Pasang paspor header persis seperti parameter perintah 'curl' suksesmu
                         this.referer = "https://www.rivestream.app/"
                         this.headers = mapOf(
                             "Origin" to "https://www.rivestream.app",
@@ -72,10 +62,10 @@ class PrimeSrcHelper {
                     })
                     linksFound++
                 } else {
-                    // Untuk server konvensional lainnya (Streamtape, Filemoon, Voe, dll.)
+                    // Jika yang tertangkap adalah cermin server alternatif (Streamtape, Voe, Filemoon, dll.)
                     val isExtractorFound = loadExtractor(
                         url = realEmbedUrl,
-                        referer = embedUrl,
+                        referer = data,
                         subtitleCallback = subtitleCallback,
                         callback = callback
                     )
@@ -86,7 +76,7 @@ class PrimeSrcHelper {
                             name = "Mirror Video",
                             url = realEmbedUrl
                         ) {
-                            this.referer = embedUrl
+                            this.referer = data
                         })
                     }
                     linksFound++
