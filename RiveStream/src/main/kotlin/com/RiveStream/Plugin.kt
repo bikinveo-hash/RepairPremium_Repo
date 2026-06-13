78681b4bbc985f78deea7465e2ab90377f2bf137
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-06-14 00:20:03 +0900

    Update Plugin.kt

--- RiveStream/src/main/kotlin/com/RiveStream/Plugin.kt
+++ RiveStream/src/main/kotlin/com/RiveStream/Plugin.kt
@@ -9,5 +9,8 @@ class RiveStreamPlugin: Plugin() {
     override fun load(context: Context) {
         // Mendaftarkan provider utama RiveStream ke sistem core
         registerMainAPI(RiveStreamProvider())
+        
+        // Mendaftarkan custom extractor super dewa untuk bypass Streamtape (tpead.net)
+        registerExtractorAPI(TpeadExtractor())
     }
 }
