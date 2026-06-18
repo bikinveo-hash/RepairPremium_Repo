04b00cc3afaceba43db56a32fd9de07f5099c139
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-06-18 09:46:18 +0900

    Update Plugin.kt

--- RiveStream/src/main/kotlin/com/RiveStream/Plugin.kt
+++ RiveStream/src/main/kotlin/com/RiveStream/Plugin.kt
@@ -7,13 +7,7 @@ import android.content.Context
 @CloudstreamPlugin
 class RiveStreamPlugin : Plugin() {
     override fun load(context: Context) {
-        // Provider utama: scraper TMDB + PrimeSrc engine
+        // Daftarkan provider utama: scraper TMDB + PrimeSrc engine untuk load link
         registerMainAPI(RiveStreamProvider())
-
-        // Extractor untuk embed URL yang datang dari loadExtractor()
-        registerExtractorAPI(RiveVidara())       // vidara.so/e/...
-        registerExtractorAPI(RiveVidsST())       // vids.st/e/...
-        registerExtractorAPI(RiveSavefiles())    // savefiles.com/e/...
-        registerExtractorAPI(RiveLizer())        // lizer123.site/getm3u8/... ← NEW
     }
 }
