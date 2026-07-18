98de0989de734a12e37d191897d84514da215ca9
bikinveo-hash <bikinveo@gmail.com>
2026-07-19 06:57:30 +0900

    Create build.gradle.kts

---
+++ OppaDrama/build.gradle.kts
@@ -0,0 +1,23 @@
+version = 2
+
+cloudstream {
+    description = "OppaDrama — Streaming Drama Korea, Movie and TV Series"
+    language = "id"
+    authors = listOf("Aldry84")
+
+    /**
+     * Status int as the following:
+     * 0: Down
+     * 1: Ok
+     * 2: Slow
+     * 3: Beta only
+     * */
+    status = 1
+    tvTypes = listOf(
+        "AsianDrama",
+        "TvSeries",
+        "Movie",
+    )
+
+    iconUrl = "https://t2.gstatic.com/faviconV2?client=SOCIAL&type=FAVICON&fallback_opts=TYPE,SIZE,URL&url=https://oppa.biz&size=128"
+}
