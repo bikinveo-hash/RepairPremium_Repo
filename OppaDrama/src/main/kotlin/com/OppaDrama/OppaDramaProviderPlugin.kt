a0e2e5b43db2aafffa4fc341c138ef8289feb451
trinityzanetamanu-code <trinity.zanetamanu@gmail.com>
2026-07-19 02:23:47 +0900

    Update OppaDramaProviderPlugin.kt

--- OppaDrama/src/main/kotlin/com/OppaDrama/OppaDramaProviderPlugin.kt
+++ OppaDrama/src/main/kotlin/com/OppaDrama/OppaDramaProviderPlugin.kt
@@ -1,18 +1,13 @@
 package com.OppaDrama
 
-import android.content.Context
 import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
 import com.lagradost.cloudstream3.plugins.Plugin
+import android.content.Context
 
 @CloudstreamPlugin
-class OppaDramaProviderPlugin : Plugin() {
+class OppaDramaPlugin: Plugin() {
     override fun load(context: Context) {
-        // Main provider
+        // Mendaftarkan provider utama ke dalam ekosistem Cloudstream
         registerMainAPI(OppaDramaProvider())
-
-        // Custom extractors (overrides/aliases for sites used by oppadrama)
-        registerExtractorAPI(Smoothpre())        // alias for vidhidepro (EarnVids)
-        registerExtractorAPI(BuzzServer())        // buzzheavier.com direct download
-        registerExtractorAPI(EmturbovidExtractor()) // emturbovid.com m3u8 resolver
     }
 }
